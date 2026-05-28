79bd9410a90b79929d119288c21fca676bee4c54
RAGNAR303 <goncalves303.gt@gmail.com>
2026-05-28 18:29:12 -0300

    teste menu sidebar

--- src/app/dashboard/components/sidebar/index.tsx
+++ src/app/dashboard/components/sidebar/index.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
 import { IoFileTrayStacked } from "react-icons/io5";
+import { TiThMenu } from "react-icons/ti";
 export function SideBar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex md:p-5 p-2 bg-slate-700/35  w-full max-w-12 md:max-w-18 hover:max-w-50 active:max-w-50 min-h-[calc(100vh-(--spacing(12.5)))] hover:h-full active:h-full rounded-br-2xl rounded-tr-2xl shadow-2xl  shadow-black/50 drop-shadow drop-shadow-black/30 overflow-hidden duration-200 group hover:absolute active:absolute  z-30 fixed  backdrop-blur-2xl">
+    <div
+      className={`flex flex-col md:p-5 p-2 bg-slate-700/35 items-center hover:items-start active:items-start w-full max-w-12 md:max-w-18 hover:max-w-50 min-h-[calc(100vh-(--spacing(12.5)))] hover:h-full active:h-full rounded-br-2xl rounded-tr-2xl shadow-2xl  shadow-black/50 drop-shadow drop-shadow-black/30 overflow-hidden duration-200 group hover:absolute active:absolute  z-30 fixed  backdrop-blur-2xl active:max-w-50  ${open && `active:max-w-50 active:h-full active:absolute  `}`}
+    >
+      <button
+        onClick={() => setOpen(!open)}
+        className="block md:hidden mt-4 hover:text-blue-700 active:text-blue-700   duration-200"
+      >
+        <TiThMenu className="text-3xl" />
+      </button>
+
       <nav className=" flex gap-6 flex-col uppercase font-bold mt-8">
         <Link
           href={"/dashboard"}
